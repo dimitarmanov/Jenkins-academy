cbd4d2f16f31e5d24c32f2a147c6d964e0bec25c
Dimitar Manov <dimitar_manov@flutterint.com>
2023-06-01 11:00:10 +0300

    Jenkins projects

---
+++ nodejs-my-proj/src/index.js
@@ -0,0 +1,30 @@
+// File: src/index.js
+
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const html = `
+    <html>
+      <head>
+        <style>
+          body {
+            background-color: white;
+          }
+        </style>
+      </head>
+      <body>
+        <h2>!! DevOps !!</h2>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
